70e0f255207f0e9905749cd5339fa07b13f6ba21
Rasmus Eneman <pie.or.paj@gmail.com>
2016-08-27 11:55:15 +0200

    feat: Sepparate build tool and compiler, create inital tests

---
+++ test/baselines/test/cases/expressions/binaryOperators/arithmeticOperator/arithmeticOperator.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+'use strict';
+
+var re1 = a + a;
+var re2 = a + b;
+var re3 = a + 0;
+var re4 = 0 + a;
+var re5 = 0 + 0;
+var re6 = b + 0;
+var re7 = 0 + b;
+var re8 = b + b;
